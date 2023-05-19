03af0313f8bcfbb7fe42ecb72ea0e8abcaf1bf7b
Hamtix1 <86091869+Hamtix1@users.noreply.github.com>
2023-05-19 11:02:53 -0500

    El filtro notch está listo

--- notch_optimo.m
+++ notch_optimo.m
@@ -1,10 +1,26 @@
-function [estimada] = notch_optimo(senal,ruido)
-    g_prom = mean(senal,"all");
-    n_prom = mean(ruido,"all");
-    gn_prom = mean(senal.*ruido,"all");
-    n2_prom = mean(ruido.^2,"all");
-    w = (gn_prom - g_prom.*n_prom)./(n2_prom - n_prom.^2);
-    disp("Factor = "+w);
-    estimada = senal - w.*ruido;
+function [estimada] = notch_optimo(senal,ruido,filas,columnas)
+%****************Cambiar formato************************
+senal=double(senal);
+ruido=double(ruido);
+%********Tamaño de los vecindarios*********************
+[m,n]=size(senal);
+vecindarioFilas=round(m/filas);
+vecindarioColumnas=round(n/columnas);
+disp("filas: "+m+" Columnas: "+n);
+
+for i=1:vecindarioFilas:m
+    for j=1:vecindarioColumnas:n        
+        g=senal(i:i+vecindarioFilas-1,j:j+vecindarioColumnas-1);
+        noise=ruido(i:i+vecindarioFilas-1,j:j+vecindarioColumnas-1);
+        g_prom = mean(g,"all");
+        n_prom = mean(noise,"all");
+        gn_prom = mean(g.*noise,"all");
+        n2_prom = mean(noise.^2,"all");
+        w = (gn_prom - g_prom.*n_prom)./(n2_prom - n_prom.^2);
+        disp("Factor = "+w);
+        estimada(i:i+vecindarioFilas-1,j:j+vecindarioColumnas-1) = g - w.*noise;
+    end
+end
+
 end
 
